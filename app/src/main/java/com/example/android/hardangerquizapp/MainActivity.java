167e46c08ef8fd40fb29a4ea1333d79484020a82
Emily Claire Gok Lucas <eclucas@calpoly.edu>
2017-08-01 11:40:55 -0700

    Gave edittext an id. Created and added the showScore method to the submit button.

--- app/src/main/java/com/example/android/hardangerquizapp/MainActivity.java
+++ app/src/main/java/com/example/android/hardangerquizapp/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.android.hardangerquizapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Calculates and shows the users name and score as a toast message
+     *
+     * @param view
+     */
+
+    public void showScore(View view){
+        EditText editText = (EditText) findViewById(R.id.name);
+        String name = editText.getText().toString();
+        String msg = "Congratulations " + name + "!";
+
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT)
+                .show();
     }
 }
