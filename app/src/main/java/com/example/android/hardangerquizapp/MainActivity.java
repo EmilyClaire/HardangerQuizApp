2ba88a372fb9f138a485642debb8195672fc2fcf
Emily Claire Gok Lucas <eclucas@calpoly.edu>
2017-08-04 00:22:11 -0700

    Added scoring functionality for question 1.

--- app/src/main/java/com/example/android/hardangerquizapp/MainActivity.java
+++ app/src/main/java/com/example/android/hardangerquizapp/MainActivity.java
@@ -4,9 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
+import static android.R.attr.checked;
+
 public class MainActivity extends AppCompatActivity {
+    private int[] questions = {0, 0, 0, 0};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,9 +28,38 @@ public class MainActivity extends AppCompatActivity {
         EditText editText = (EditText) findViewById(R.id.name);
         String name = editText.getText().toString();
         String msg = "Congratulations " + name + "!";
+        msg += "\nYou scored " + sumScore() + " / " + questions.length + " !";
 
         Toast.makeText(this, msg, Toast.LENGTH_SHORT)
                 .show();
+    }
+
+    private int sumScore() {
+        int sum = 0;
+
+        for (int i = 0; i < questions.length; i++) {
+            sum += questions[i];
+        }
+
+        return sum;
+    }
+
+
+    /**
+     * Sets the point value for question 1 as 1 if correct or 0 if incorrect.
+     *
+     * @param view
+     */
+    public void scoreQuestion1(View view) {
+        RadioButton button = ((RadioButton) view);
+
+        if (button.isChecked()) {
+            if (view.getId() == R.id.q_1_doves) {
+                questions[0] = 1;
+            } else {
+                questions[0] = 0;
+            }
+        }
     }
 
     @Override
@@ -36,6 +69,7 @@ public class MainActivity extends AppCompatActivity {
         EditText editText = (EditText) findViewById(R.id.name);
         String name = editText.getText().toString();
 
+        savedInstanceState.putIntArray("questions", questions);
         savedInstanceState.putString("name", name);
     }
 
@@ -46,8 +80,8 @@ public class MainActivity extends AppCompatActivity {
         // This bundle has also been passed to onCreate.
 
         EditText editText = (EditText) findViewById(R.id.name);
+        questions = savedInstanceState.getIntArray("questions");
         String name = savedInstanceState.getString("name");
-
         editText.setText(name);
     }
 
